cb4ed0d7ef315da165d93500f7d8e766f2e3a1e7
Николай Разилов <nickraz@yandex.ru>
2020-03-03 14:34:57 +0300

    Заполняю Intro

---
+++ src/Intro/Button.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import css from './Button.module.css';
+
+const Button = () => {
+    return (
+        <a href="#" className={css.href}>Заказать букет</a>
+    );
+}
+
+export default Button;
--- src/Intro/Intro.jsx
+++ src/Intro/Intro.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import Button from './Button.jsx'
 import css from './Intro.module.css';
 
 const Intro = () => {
     return (
-        <div class={`row ${css.intro}`} data-container="container">
-
+        <div class={`container ${css.intro}`} data-container="container">
+            <div className="col-sm-6">
+                <h1 className={css.bigwhite}>Магазин цветов Махачкала</h1>
+                <p className={css.subtitle}>Доставка цветов и букетов</p>
+                <h3 className={css.smallwhite}>
+                    Цветочный интернет-магазин на Первомайской, 172. Доставка цветов в Пятигорске, Ессентуках, Железноводске, по КавМинВодам.
+                </h3>
+                <br/>
+                <Button />
+            </div>
         </div>
     );
 }
